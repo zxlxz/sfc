a800909efd5510c2d4dc50ad175dd43de7215572
zxlxz <zhuxlx@126.com>
2026-04-19 03:05:13 +0800

    refactor(core): strengthen num utilities with consteval and inline hints
    - Change max_value/min_value to consteval for compile-time enforcement
    - Add [[gnu::always_inline]] to abs, saturating_sub, align_up, next_power_of_two
    - Add align_up utility for memory alignment calculations
    - Reorder saturating_sub before next_power_of_two for logical grouping

--- src/sfc/core/num.h
+++ src/sfc/core/num.h
@@ -18,7 +18,7 @@ template <class T>
 concept float_ = same_<T, float> || same_<T, double>;
 
 template <int_ T>
-static constexpr auto max_value() -> T {
+consteval auto max_value() -> T {
   if constexpr (uint_<T>) {
     return static_cast<T>(~T{0});
   } else {
@@ -27,17 +27,28 @@ static constexpr auto max_value() -> T {
 }
 
 template <sint_ T>
-static constexpr auto min_value() -> T {
+consteval auto min_value() -> T {
   return static_cast<T>(T{1} << (sizeof(T) * 8 - 1));
 }
 
 template <int_ T>
-constexpr auto abs(T val) -> T {
+[[gnu::always_inline]] constexpr auto abs(T val) -> T {
   return val >= 0 ? val : 0 - val;
 }
 
 template <uint_ T>
-constexpr auto next_power_of_two(T n) -> T {
+[[gnu::always_inline]] constexpr auto saturating_sub(T a, T b) -> T {
+  return a < b ? 0U : a - b;
+}
+
+template <uint_ T>
+[[gnu::always_inline]] constexpr auto align_up(T val, T align) -> T {
+  const auto mask = T{align - 1};
+  return (val + mask) & ~mask;
+}
+
+template <uint_ T>
+[[gnu::always_inline]] constexpr auto next_power_of_two(T n) -> T {
   if ((n & (n - 1)) == 0) {
     return n;
   }
@@ -46,11 +57,6 @@ constexpr auto next_power_of_two(T n) -> T {
     res <<= 1;
   }
   return res;
-}
-
-template <uint_ T>
-constexpr auto saturating_sub(T a, T b) -> T {
-  return a < b ? 0 : a - b;
 }
 
 auto flt_eq_ulp(f64 a, f64 b, u32 ulp = 4) noexcept -> bool;
