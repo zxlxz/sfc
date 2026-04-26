f02f0f110a9c948b177519a39d00a5eedb7d6f92
zxlxz <zhuxlx@126.com>
2026-04-26 21:38:57 +0800

    feat(core): extend Tuple to support up to 16 elements and add indexed accessors
    - Expand Inner specializations from 10 to 16 parameters
    - Add get<I>() and get_mut<I>() for compile-time indexed element access
    - Extend map/map_mut to cover all 16 elements

--- src/sfc/core/tuple.h
+++ src/sfc/core/tuple.h
@@ -40,11 +40,29 @@ struct Inner<A,B,C,D,E,F,G,H,I> { A _0; B _1; C _2; D _3; E _4; F _5; G _6; H _7
 
 template <class A, class B, class C, class D, class E, class F, class G, class H, class I, class J>
 struct Inner<A,B,C,D,E,F,G,H,I,J> { A _0; B _1; C _2; D _3; E _4; F _5; G _6; H _7; I _8; J _9; };
+
+template<class A, class B, class C, class D, class E, class F, class G, class H, class I, class J, class K>
+struct Inner<A,B,C,D,E,F,G,H,I,J,K> { A _0; B _1; C _2; D _3; E _4; F _5; G _6; H _7; I _8; J _9; K _10; };
+
+template<class A, class B, class C, class D, class E, class F, class G, class H, class I, class J, class K, class L>
+struct Inner<A,B,C,D,E,F,G,H,I,J,K,L> { A _0; B _1; C _2; D _3; E _4; F _5; G _6; H _7; I _8; J _9; K _10; L _11; };
+
+template<class A, class B, class C, class D, class E, class F, class G, class H, class I, class J, class K, class L, class M>
+struct Inner<A,B,C,D,E,F,G,H,I,J,K,L,M> { A _0; B _1; C _2; D _3; E _4; F _5; G _6; H _7; I _8; J _9; K _10; L _11; M _12; };
+
+template<class A, class B, class C, class D, class E, class F, class G, class H, class I, class J, class K, class L, class M, class N>
+struct Inner<A,B,C,D,E,F,G,H,I,J,K,L,M,N> { A _0; B _1; C _2; D _3; E _4; F _5; G _6; H _7; I _8; J _9; K _10; L _11; M _12; N _13; };
+
+template<class A, class B, class C, class D, class E, class F, class G, class H, class I, class J, class K, class L, class M, class N, class O>
+struct Inner<A,B,C,D,E,F,G,H,I,J,K,L,M,N,O> { A _0; B _1; C _2; D _3; E _4; F _5; G _6; H _7; I _8; J _9; K _10; L _11; M _12; N _13; O _14; };
+
+template<class A, class B, class C, class D, class E, class F, class G, class H, class I, class J, class K, class L, class M, class N, class O, class P>
+struct Inner<A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P> { A _0; B _1; C _2; D _3; E _4; F _5; G _6; H _7; I _8; J _9; K _10; L _11; M _12; N _13; O _14; P _15; };
 // clang-format on
 
 template <class... T>
 struct Tuple : Inner<T...> {
-  static_assert(sizeof...(T) <= 10, "Tuple: only support up to 10 elements");
+  static_assert(sizeof...(T) <= 16, "Tuple: only support up to 16 elements");
   static constexpr u32 N = sizeof...(T);
 
  public:
@@ -52,6 +70,46 @@ struct Tuple : Inner<T...> {
   ~Tuple() = default;
 
  public:
+  template <u32 I>
+  auto get() const -> const auto& {
+    if constexpr (I == 0) return this->_0;
+    if constexpr (I == 1) return this->_1;
+    if constexpr (I == 2) return this->_2;
+    if constexpr (I == 3) return this->_3;
+    if constexpr (I == 4) return this->_4;
+    if constexpr (I == 5) return this->_5;
+    if constexpr (I == 6) return this->_6;
+    if constexpr (I == 7) return this->_7;
+    if constexpr (I == 8) return this->_8;
+    if constexpr (I == 9) return this->_9;
+    if constexpr (I == 10) return this->_10;
+    if constexpr (I == 11) return this->_11;
+    if constexpr (I == 12) return this->_12;
+    if constexpr (I == 13) return this->_13;
+    if constexpr (I == 14) return this->_14;
+    if constexpr (I == 15) return this->_15;
+  }
+
+  template <u32 I>
+  auto get_mut() -> auto& {
+    if constexpr (I == 0) return this->_0;
+    if constexpr (I == 1) return this->_1;
+    if constexpr (I == 2) return this->_2;
+    if constexpr (I == 3) return this->_3;
+    if constexpr (I == 4) return this->_4;
+    if constexpr (I == 5) return this->_5;
+    if constexpr (I == 6) return this->_6;
+    if constexpr (I == 7) return this->_7;
+    if constexpr (I == 8) return this->_8;
+    if constexpr (I == 9) return this->_9;
+    if constexpr (I == 10) return this->_10;
+    if constexpr (I == 11) return this->_11;
+    if constexpr (I == 12) return this->_12;
+    if constexpr (I == 13) return this->_13;
+    if constexpr (I == 14) return this->_14;
+    if constexpr (I == 15) return this->_15;
+  }
+
   void map(auto&& f) const {
     if constexpr (N > 0) f(this->_0);
     if constexpr (N > 1) f(this->_1);
@@ -63,6 +121,12 @@ struct Tuple : Inner<T...> {
     if constexpr (N > 7) f(this->_7);
     if constexpr (N > 8) f(this->_8);
     if constexpr (N > 9) f(this->_9);
+    if constexpr (N > 10) f(this->_10);
+    if constexpr (N > 11) f(this->_11);
+    if constexpr (N > 12) f(this->_12);
+    if constexpr (N > 13) f(this->_13);
+    if constexpr (N > 14) f(this->_14);
+    if constexpr (N > 15) f(this->_15);
   }
 
   void map_mut(auto&& f) {
@@ -76,6 +140,12 @@ struct Tuple : Inner<T...> {
     if constexpr (N > 7) f(this->_7);
     if constexpr (N > 8) f(this->_8);
     if constexpr (N > 9) f(this->_9);
+    if constexpr (N > 10) f(this->_10);
+    if constexpr (N > 11) f(this->_11);
+    if constexpr (N > 12) f(this->_12);
+    if constexpr (N > 13) f(this->_13);
+    if constexpr (N > 14) f(this->_14);
+    if constexpr (N > 15) f(this->_15);
   }
 
   void fmt(auto& f) const {
