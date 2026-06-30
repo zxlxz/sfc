2043b1622b124e03f634614997567eceec2daa92
zxlxz <zhuxlx@126.com>
2026-06-30 10:42:15 +0800

    refactor(slice): remove redundant Slice<const T> specialization and add conversion operator

--- src/sfc/core/io.cc
+++ src/sfc/core/io.cc
@@ -60,18 +60,6 @@ auto Slice<T>::read(Slice<u8> buf) noexcept -> io::Result<usize> {
   return io::Result<usize>{amt};
 }
 
-template <class T>
-auto Slice<const T>::read(Slice<u8> buf) noexcept -> io::Result<usize> {
-  if (_len == 0 || buf._len == 0) {
-    return io::Result<usize>{0UZ};
-  }
-  const auto amt = _len < buf._len ? _len : buf._len;
-  ptr::copy_nonoverlapping(_ptr, buf._ptr, amt);
-  _ptr += amt;
-  _len -= amt;
-  return io::Result<usize>{amt};
-}
-
 template auto Slice<u8>::read(Slice<u8> buf) noexcept -> io::Result<usize>;
 template auto Slice<const u8>::read(Slice<u8> buf) noexcept -> io::Result<usize>;
 
