308aa22a8548a2eda31af8e966f53b8c678ebaf7
zxlxz <zhuxlx@126.com>
2026-07-06 02:03:27 +0800

    refactor(test): enhance GTestCase and GTestSuite structure for clarity and functionality

--- src/sfc/test/gtest.cc
+++ src/sfc/test/gtest.cc
@@ -12,265 +12,284 @@ enum class GTestResult {
   Failed,
 };
 
-struct GTestCase {
+class GTestCase {
   Test _test;
-  GTestResult _result = GTestResult::None;
+  Str _suite_name;
+  Str _test_name;
+  GTestResult _result{};
+  usize _duration_ms{};
 
  public:
-  auto name() const -> Str {
-    return _test.name();
+  GTestCase(Test test) : _test{test} {
+    _suite_name = _test.mod();
+    _test_name = _test.name();
   }
 
-  auto match(Str full_pat) const -> bool {
-    if (full_pat.is_empty() || full_pat == "*") {
+  auto name() const -> Str {
+    return _test_name;
+  }
+
+  auto result() const -> GTestResult {
+    return _result;
+  }
+
+  auto match(Slice<const Str> pats) const -> bool {
+    if (pats.is_empty()) {
       return true;
     }
+    return pats.iter().any([&](Str pat) { return this->match_imp(pat); });
+  }
 
-    const auto name = _test.name();
-    const auto p = full_pat.rfind('.');
-    if (!p) {
-      return name == full_pat;
+  void run(bool use_color) {
+    _result = GTestResult::None;
+    this->show_result(use_color);
+    const auto timer = time::Instant::now();
+    try {
+      _test.run();
+      _result = GTestResult::Passed;
+    } catch (...) {
+      _result = GTestResult::Failed;
     }
+    _duration_ms = timer.elapsed().as_millis();
+    this->show_result(use_color);
+  }
 
-    const auto pat = full_pat[{*p + 1, $}];
+ public:
+  void fmt(auto& f) const {
+    if (_suite_name.is_empty()) {
+      f.write_fmt("{}", _test_name);
+    } else {
+      f.write_fmt("{}.{}", _suite_name, _test_name);
+    }
+  }
+
+ private:
+  auto match_imp(Str pat) const -> bool {
     if (pat.is_empty() || pat == "*") {
       return true;
     }
 
-    if (pat == name) {
+    const auto pat_pos = pat.rfind('.');
+    const auto pat_name = pat_pos ? pat[{*pat_pos + 1, $}] : pat;
+    if (pat_name.is_empty() || pat_name == "*") {
+      return true;
+    }
+
+    if (pat_name == _test_name) {
       return true;
     }
 
     return false;
   }
 
-  auto match_pats(Slice<const Str> pats) const -> bool {
-    return pats.is_empty() || pats.iter().any([&](Str pat) { return this->match(pat); });
-  }
-
-  auto run(bool use_color) -> bool {
-    _result = GTestResult::None;
-    const auto s_run = use_color ? Str{"\033[32m[ RUN      ]\033[39m"} : Str{"[ RUN      ]"};
-    const auto s_suc = use_color ? Str{"\033[32m[       OK ]\033[39m"} : Str{"[       OK ]"};
-    const auto s_err = use_color ? Str{"\033[31m[  FAILED  ]\033[39m"} : Str{"[  FAILED  ]"};
-
-    const auto timer = time::Instant::now();
-    io::println("{} {}", s_run, *this);
-    try {
-      _test.run();
-      io::println("{} {} ({} ms)", s_suc, *this, timer.elapsed().as_millis());
-      _result = GTestResult::Passed;
-    } catch (...) {
-      io::println("{} {} ({} ms)", s_err, *this, timer.elapsed().as_millis());
-      _result = GTestResult::Failed;
-      return false;
+  void show_result(bool use_color) {
+    switch (_result) {
+      case GTestResult::None: {
+        const auto s_run = !use_color ? Str{"[ RUN      ]"} : Str{"\033[32m[ RUN      ]\033[39m"};
+        io::println("{} {}", s_run, *this);
+        break;
+      }
+      case GTestResult::Passed: {
+        const auto s_suc = !use_color ? Str{"[       OK ]"} : Str{"\033[32m[       OK ]\033[39m"};
+        io::println("{} {} ({} ms)", s_suc, *this, _duration_ms);
+        break;
+      }
+      case GTestResult::Failed: {
+        const auto s_err = !use_color ? Str{"[  FAILED  ]"} : Str{"\033[31m[  FAILED  ]\033[39m"};
+        io::println("{} {} ({} ms)", s_err, *this, _duration_ms);
+        break;
+      }
     }
-
-    return true;
-  }
-
-  void fmt(auto& f) const {
-    f.write_fmt("{}.{}", _test.mod(), _test.name());
   }
 };
 
-struct GTestSuite {
+class GTestSuite {
   const Module& _mod;  // Module is global and never modified, so reference is safe
   List<GTestCase> _cases{};
+  usize _run_cnt{0};
+  usize _passed_cnt{0};
+  usize _failed_cnt{0};
+  usize _duration_ms{};
 
  public:
-  auto match(Str pats) const -> bool {
-    const auto pats_len = pats.len();
+  GTestSuite(const Module& mod) : _mod{mod} {}
 
-    if (pats_len == 0) {
-      return true;
+  void filter(Slice<const Str> pats) {
+    _cases.clear();
+
+    if (!this->match(pats)) {
+      return;
     }
 
-    const auto name = _mod.name();
-    const auto name_len = name.len();
-
-    // A::
-    if (pats_len < name_len) {
-      return pats.ends_with(':') && name.starts_with(pats);
+    for (auto& test : _mod.tests()) {
+      auto test_case = GTestCase{test};
+      if (!test_case.match(pats)) continue;
+      _cases.push(test_case);
     }
-
-    // A::B
-    if (pats_len == name_len) {
-      return name == pats;
-    }
-
-    // A::B.C
-    if (pats_len > name_len) {
-      return pats[name_len] == '.' && pats.starts_with(name);
-    }
-    return false;
   }
 
-  auto match_pats(Slice<const Str> pats) const -> bool {
-    return pats.is_empty() || pats.iter().any([&](Str pat) { return this->match(pat); });
+  auto match(Slice<const Str> pats) const -> bool {
+    if (pats.is_empty()) {
+      return true;
+    }
+    return pats.iter().any([&](Str pat) { return this->match_imp(pat); });
   }
 
   auto cases() const noexcept -> Slice<const GTestCase> {
     return _cases.as_slice();
   }
 
-  void filter(Slice<const Str> pats) {
-    _cases.clear();
-    for (auto& test : _mod.tests()) {
-      auto test_case = GTestCase{test};
-      if (!this->match_pats(pats)) continue;
-      if (!test_case.match_pats(pats)) continue;
-      _cases.push(test_case);
-    }
+  auto run_cnt() const -> usize {
+    return _run_cnt;
   }
 
   auto passed_cnt() const -> usize {
-    auto cnt = usize{0U};
-    for (const auto& test_case : _cases.as_slice()) {
-      if (test_case._result == GTestResult::Passed) {
-        cnt += 1;
-      }
-    }
-    return cnt;
+    return _passed_cnt;
   }
 
   auto failed_cnt() const -> usize {
-    auto cnt = usize{0U};
-    for (const auto& test_case : _cases.as_slice()) {
-      if (test_case._result == GTestResult::Failed) {
-        cnt += 1;
-      }
-    }
-    return cnt;
+    return _failed_cnt;
   }
 
   void run(bool use_color) {
-    const auto suite_name = _mod.name();
-    const auto cases_cnt = _cases.len();
-    const auto title = use_color ? Str{"\033[32m[----------]\033[39m"} : Str{"[----------]"};
+    _run_cnt = 0U;
+    _passed_cnt = 0U;
+    _failed_cnt = 0U;
 
-    io::println("{} {} tests from {}", title, cases_cnt, suite_name);
+    this->show_run(use_color);
     const auto timer = time::Instant::now();
     for (auto& test_case : _cases.as_mut_slice()) {
+      _run_cnt++;
       test_case.run(use_color);
+      switch (test_case.result()) {
+        case GTestResult::Passed: _passed_cnt++; break;
+        case GTestResult::Failed: _failed_cnt++; break;
+        default:                  break;
+      }
     }
-    const auto dur_ms = timer.elapsed().as_millis();
-    io::println("{} {} tests from {} ({} ms total)", title, cases_cnt, suite_name, dur_ms);
-    io::println("");
+    _duration_ms = timer.elapsed().as_millis();
+    this->show_result(use_color);
   }
 
-  void show_fails(Str title) const {
+  void show_fails(bool use_color) const {
+    const auto title = !use_color ? Str{"[  FAILED  ]"} : Str{"\033[31m[  FAILED  ]\033[39m"};
     for (const auto& test_case : _cases.as_slice()) {
-      if (test_case._result == GTestResult::Failed) {
+      if (test_case.result() == GTestResult::Failed) {
         io::println("{} {}", title, test_case);
       }
     }
   }
+
+ private:
+  auto match_imp(Str pat) const -> bool {
+    const auto pat_len = pat.len();
+
+    if (pat_len == 0) {
+      return true;
+    }
+
+    const auto suite_name = _mod.name();
+
+    // A::
+    if (pat_len < suite_name.len()) {
+      return pat.ends_with(':') && suite_name.starts_with(pat);
+    }
+
+    // A::B
+    if (pat_len == suite_name.len()) {
+      return suite_name == pat;
+    }
+
+    // A::B.C
+    if (pat_len > suite_name.len()) {
+      return pat[suite_name.len()] == '.' && pat.starts_with(suite_name);
+    }
+    return false;
+  }
+
+  void show_run(bool use_color) const {
+    const auto title = !use_color ? Str{"[----------]"} : Str{"\033[32m[----------]\033[39m"};
+    io::println("{} {} tests from {}", title, _cases.len(), _mod.name());
+  }
+
+  void show_result(bool use_color) const {
+    const auto title = !use_color ? Str{"[----------]"} : Str{"\033[32m[----------]\033[39m"};
+    io::println("{} {} tests from {} ({} ms total)", title, _cases.len(), _mod.name(), _duration_ms);
+    io::println("");
+  }
 };
 
-struct GTestRun {
+class GTestRunner {
   List<GTestSuite> _suites{};
+  usize _case_cnt{};
+
+  usize _run_cnt{};
+  usize _passed_cnt{};
+  usize _failed_cnt{};
+  usize _duration_ms{};
 
  public:
-  void collect_suites(Str filter) {
-    auto pats = this->parse_filter(filter);
+  void collect_suites(Slice<const Str> pats) {
     _suites.clear();
+    _case_cnt = 0;
 
     for (auto& mod : test::modules()) {
       auto suite = GTestSuite{mod};
-      suite.filter(pats.as_slice());
-      if (suite.cases().is_empty()) {
+      suite.filter(pats);
+
+      const auto cases = suite.cases();
+      if (cases.is_empty()) {
         continue;
       }
+      _case_cnt += cases.len();
       _suites.push(mem::move(suite));
     }
   }
 
-  auto suite_cnt() const -> usize {
-    return _suites.len();
-  }
+  void run(bool use_color) {
+    _run_cnt = {};
+    _passed_cnt = {};
+    _failed_cnt = {};
+    _duration_ms = {};
 
-  auto tests_cnt() const -> usize {
-    auto cnt = usize{0U};
-    for (const auto& suite : _suites.as_slice()) {
-      cnt += suite.cases().len();
-    }
-    return cnt;
-  }
-
-  auto passed_cnt() const -> usize {
-    auto cnt = usize{0U};
-    for (const auto& suite : _suites.as_slice()) {
-      cnt += suite.passed_cnt();
-    }
-    return cnt;
-  }
-
-  auto failed_cnt() const -> usize {
-    auto cnt = usize{0U};
-    for (const auto& suite : _suites.as_slice()) {
-      cnt += suite.failed_cnt();
-    }
-    return cnt;
-  }
-
-  void run(bool color) {
-    const auto suite_cnt = this->suite_cnt();
-    const auto tests_cnt = this->tests_cnt();
-
-    const auto title = color ? Str{"\033[32m[==========]\033[39m"} : Str{"[==========]"};
-    io::println("{} Running {} tests from {} test suites.", title, tests_cnt, suite_cnt);
-
+    this->show_run(use_color);
     const auto timer = time::Instant::now();
     for (auto& suite : _suites.as_mut_slice()) {
-      suite.run(color);
+      suite.run(use_color);
+      _run_cnt += suite.run_cnt();
+      _passed_cnt += suite.passed_cnt();
+      _failed_cnt += suite.failed_cnt();
     }
-    const auto dur_ms = timer.elapsed().as_millis();
-    io::println("{} {} tests from {} test suites ran. ({} ms total)", title, tests_cnt, suite_cnt, dur_ms);
-    this->show_summary(color);
+    _duration_ms = timer.elapsed().as_millis();
+    this->show_result(use_color);
+    this->show_summary(use_color);
   }
 
  private:
-  auto parse_filter(Str filter) -> List<Str> {
-    auto find_pos = [](Str s) -> usize {
-      for (auto p = 0U; p < s._len; p++) {
-        switch (s[p]) {
-          case ';': return p;
-          case ':':
-            if (s[p + 1] != ':') return p;  // pass '::'
-            p += 1;
-        }
-      }
-      return s._len;
-    };
-
-    auto pats = List<Str>{};
-    while (!filter.is_empty()) {
-      const auto pos = find_pos(filter);
-      pats.push(filter[{0, pos}]);
-      filter = filter[{pos + 1, $}];
-    }
-    return pats;
+  void show_run(bool use_color) const {
+    const auto title = !use_color ? Str{"[==========]"} : Str{"\033[32m[==========]\033[39m"};
+    io::println("{} Running {} tests from {} test suites.", title, _case_cnt, _suites.len());
   }
 
-  void show_summary(bool color) {
-    const auto passed_title = color ? Str{"\033[32m[  PASSED  ]\033[39m"} : Str{"[  PASSED  ]"};
-    const auto failed_title = color ? Str{"\033[31m[  FAILED  ]\033[39m"} : Str{"[  FAILED  ]"};
+  void show_result(bool use_color) const {
+    const auto title = !use_color ? Str{"[==========]"} : Str{"\033[32m[==========]\033[39m"};
+    io::println("{} {} tests from {} test suites ran. ({} ms total)", title, _case_cnt, _suites.len(), _duration_ms);
+  }
 
-    const auto passed_cnt = this->passed_cnt();
-    io::println("{} {} tests.", passed_title, passed_cnt);
+  void show_summary(bool use_color) {
+    const auto pass_title = !use_color ? Str{"[  PASSED  ]"} : Str{"\033[32m[  PASSED  ]\033[39m"};
+    const auto fail_title = !use_color ? Str{"[  FAILED  ]"} : Str{"\033[31m[  FAILED  ]\033[39m"};
 
-    const auto failed_cnt = this->failed_cnt();
-    if (failed_cnt == 0) {
-      return;
+    io::println("{} {} tests.", pass_title, _passed_cnt);
+
+    if (_failed_cnt != 0) {
+      io::println("{} {} tests. listed below:", fail_title, _failed_cnt);
+      for (const auto& suite : _suites.as_slice()) {
+        suite.show_fails(use_color);
+      }
+      io::println("");
+      io::println("{} FAILED TESTS", _failed_cnt);
     }
-    io::println("{} {} tests. listed below:", failed_title, failed_cnt);
-    for (const auto& suite : _suites.as_slice()) {
-      suite.show_fails(failed_title);
-    }
-
-    io::println("");
-    io::println("{} FAILED TESTS", failed_cnt);
   }
 };
 
@@ -338,9 +357,34 @@ void gtest_list_tests(Str uri) {
   test::gtest_list_tests_xml(path);
 }
 
+auto gtest_split_pats(Str filter) -> List<Str> {
+  auto find_pos = [](Str s) -> usize {
+    for (auto p = 0U; p < s._len; p++) {
+      switch (s[p]) {
+        case ';': return p;
+        case ':':
+          if (s[p + 1] != ':') return p;  // pass '::'
+          p += 1;
+      }
+    }
+    return s._len;
+  };
+
+  auto pats = List<Str>{};
+  while (!filter.is_empty()) {
+    const auto pos = find_pos(filter);
+    pats.push(filter[{0, pos}]);
+    filter = filter[{pos + 1, $}];
+  }
+
+  return pats;
+}
+
 void gtest_run(Str filter, bool color) {
-  auto runner = GTestRun{};
-  runner.collect_suites(filter);
+  const auto pats = gtest_split_pats(filter);
+
+  auto runner = GTestRunner{};
+  runner.collect_suites(pats.as_slice());
   runner.run(color);
 }
 
